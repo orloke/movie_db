6a1ac5827cebcc94a56fd774fb23f10b51097db3
Junior Dering <juniordering@hotmail.com>
2025-10-28 21:00:45 -0300

    fix: correct name in pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from 'react-router';
-import { Home } from './pages/home';
+import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
 import { MainLayout } from './layouts/MainLayout';
-import { Movie } from './pages/movie';
+import { Movie } from './pages/Movie';
+import { Favorites } from './pages/Favorites';
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
         <Route path="/movie/:id" element={<Movie />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,5 @@
+export const Favorites = () => {
+  return (
+    <div className=''></div>
+  );
+};
